8fcc3cb5b5e11fd5e1d2a307f2ae3f6666d822f0
Jan Hannig <janhannig210705@gmail.com>
2025-03-24 16:28:42 +0100

    Update Adresse.java

--- Programmieren 2/Lektion14/Adresse.java
+++ Programmieren 2/Lektion14/Adresse.java
@@ -6,10 +6,24 @@ public class Adresse {
     int plz;
     String ort;
 
+ public static void checkUppercase(String letter) {
+        char c = letter.charAt(0);
+        if (c < 'A' || c > 'Z') {
+            throw new RuntimeException("Not an uppercase letter");
+        }
+    }
+
+    public static void checkHausnummer(String number) {
+        char c = number.charAt(0);
+        if (c < '0' || '9' < c) {
+            throw new RuntimeException("Not a number");
+        }
+    }
+
     public Adresse(String strasse, String hausnummer, int plz, String ort) {
-            this.strasse = strasse;
-            this.hausnummer = hausnummer;
+            checkUppercase(strasse);
+            checkHausnummer(hausnummer);
             this.plz = plz;
-            this.ort = ort;
+            checkUppercase(ort);
         }
     }
